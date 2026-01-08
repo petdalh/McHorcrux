9ed7d0bf6940a7b26cbe8650032d4541fad6685d
Petter Dalhaug <124355644+petdalh@users.noreply.github.com>
2026-01-08 11:10:15 -0600

    added temporal operators

--- numpy_core/colregs_stl/stl_formula.py
+++ numpy_core/colregs_stl/stl_formula.py
@@ -1,3 +1,6 @@
-class STLFormula:
+from abc import ABC, abstractmethod
+
+class STLFormula(ABC):
+    @abstractmethod
     def evaluate_robust(self, joint_state, k):
         raise NotImplementedError
