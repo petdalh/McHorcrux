597a22140f4a13dc1ba78e687405bcad0f2ee89d
Petter Dalhaug <124355644+petdalh@users.noreply.github.com>
2026-01-09 11:02:27 -0600

    IA implication added to stl core

--- numpy_core/colregs_stl/stl_core.py
+++ numpy_core/colregs_stl/stl_core.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class STLFormula(ABC):
     @abstractmethod
     def evaluate_robust(self, joint_state, k: int) -> float:
-        """Returns the robustness value rho at time step k[cite: 109]."""
+        """Returns the robustness value rho at time step k."""
         pass
 
 # Boolean Operators
@@ -42,3 +42,17 @@ class Eventually(STLFormula):
     def evaluate_robust(self, joint_state, k):
         return np.max([self.formula.evaluate_robust(joint_state, i) 
                       for i in range(k + self.a, k + self.b + 1)])
+
+class IA_Implication(STLFormula):
+    def __init__(self, antecedent: STLFormula, consequent: STLFormula):
+        self.ante = antecedent
+        self.cons = consequent
+
+    def evaluate_robust(self, joint_state, k: int):
+        # Measuring the 'Trigger'
+        rho_in = self.ante.evaluate_robust(joint_state, k)
+        
+        # Measuring the 'Requirement'
+        rho_out = self.cons.evaluate_robust(joint_state, k)
+        
+        return rho_in, rho_out
